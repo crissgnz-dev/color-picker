21bec6dee2990f1f88c94e1e38499e00cb45843a
crissgnz-dev <gonzalez.cristian@tecnica7.edu.ar>
2025-07-30 21:19:48 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Logo from './assets/colorpicker.png'
+
+function App() {
+
+  return (
+    <>
+      <main className='mainApp'>
+        <section>
+          <img src={Logo} alt="logo" className='logo'/>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
